5d7dc588d1da31a2e6c928c9042151486f106f88
Emory Petermann <emory@heyzap.com>
2016-01-08 15:39:50 -0800

    allow user to create new hackday

--- app/controllers/hackdays_controller.rb
+++ app/controllers/hackdays_controller.rb
@@ -2,4 +2,27 @@ class HackdaysController < ApplicationController
   def index
     @hackdays = Hackday.all
   end
+
+  def show
+    @hackday = Hackday.find(params[:id])
+  end
+
+  def new
+    @hackday = Hackday.new
+  end
+
+  def create
+    @hackday = Hackday.new(hackday_params)
+    if @hackday.save
+      redirect_to @hackday
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def hackday_params
+    params.require(:hackday).permit(:name, :start, :end)
+  end
 end
