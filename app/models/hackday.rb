42ed3350105f561bcf87e3ddf6e24360d6602ad3
Emory Petermann <emory@heyzap.com>
2016-01-11 14:05:17 -0800

    add some more validations for creating projects and hackdays

--- app/models/hackday.rb
+++ app/models/hackday.rb
@@ -1,3 +1,7 @@
 class Hackday < ActiveRecord::Base
   has_many :projects
+
+  validates :name, presence: true
+  validates :start, presence: true
+  validates :end, presence: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :votes
   belongs_to :hackday
+
+  validates :name, presence: true
 end
