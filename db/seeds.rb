68b1b360086f34245f313ad55a6d35cf5d89dddf
Emory Petermann <emory@heyzap.com>
2016-01-11 15:17:28 -0800

    use local variables in db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 3.times do |d|
-  @hackday = Hackday.create(start: DateTime.now, end: DateTime.now + 1, name: "#{Faker::Hacker.ingverb} #{Faker::Hacker.ingverb} #{Faker::Hacker.ingverb}")
+  hackday = Hackday.create(start: DateTime.now, end: DateTime.now + 1, name: "#{Faker::Hacker.ingverb} #{Faker::Hacker.ingverb} #{Faker::Hacker.ingverb}")
   10.times do |n|
     name = "#{Faker::Hacker.adjective} #{Faker::Hacker.abbreviation} #{Faker::Hacker.noun}"
-    @project = @hackday.projects.create!(name: name)
+    project = hackday.projects.create!(name: name)
     3.times do |i|
-      @vote = @project.votes.create!(user: User.create!(username: Faker::Internet.user_name, password: Faker::Internet.password))
-      @vote.save
+      vote = project.votes.create!(user: User.create!(username: Faker::Internet.user_name, password: Faker::Internet.password))
+      vote.save
     end
   end
 end
